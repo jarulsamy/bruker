339e5142ef8736c1841ca525ab9a902b0a463fcb
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2025-11-09 15:39:08 -0700

    Add metadata to executable

--- powder-plot/build.rs
+++ powder-plot/build.rs
@@ -4,6 +4,11 @@ fn main() -> io::Result<()> {
     if cfg!(target_os = "windows") {
         WindowsResource::new()
             .set_icon("assets/icon.ico")
+            .set("FileDescription", env!("CARGO_PKG_DESCRIPTION"))
+            .set("ProductName", env!("CARGO_PKG_NAME"))
+            .set("FileVersion", env!("CARGO_PKG_VERSION"))
+            .set("ProductVersion", env!("CARGO_PKG_VERSION"))
+            .set("LegalCopyright", &format!("© 2025 {}", env!("CARGO_PKG_AUTHORS")))
             .compile()?;
     }
     Ok(())
